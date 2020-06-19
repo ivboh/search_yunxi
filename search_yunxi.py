b44b4b87fc47dcc0534a884c11de33ed28b48c37
ivboh <xhuang1202@gmail.com>
2020-06-18 19:27:02 -0500

    erge file input

--- search_yunxi.py
+++ search_yunxi.py
@@ -8,15 +8,14 @@ import numpy as np
 import io
 import datetime
 
-def scrape(logger_path):
+def read_link(URL):
+  url = urlopen(URL)
+  return [line.decode('utf-8') for line in url]
+
+def scrape(logger_path , search_list):
   '''
   open webpage with given URL list.
   '''
-  search_list = ["https://v.qq.com/x/search/?q=%E6%9C%88%E4%B8%8A%E9%87%8D%E7%81%AB", \
-                 "https://s.weibo.com/weibo/%25E6%259C%2588%25E4%25B8%258A%25E9%2587%258D%25E7%2581%25AB?topnav=1&wvr=6&b=1",\
-                 "https://s.weibo.com/weibo/%E7%BD%97%E4%BA%91%E7%86%99Leo?topnav=1&wvr=6&topsug=1",\
-                 "https://so.youku.com/search_video/q_%E6%9C%88%E4%B8%8A%E9%87%8D%E7%81%AB",
-                 "https://so.iqiyi.com/so/q_%E6%9C%88%E4%B8%8A%E9%87%8D%E7%81%AB?source=suggest&sr=577017943149s_sr%3D1&s_token=suggest_4#f4fe07ba4179b5a9614a5e0bfd04d325#0#yue"]
   logger = open(logger_path, 'a+')
   logger.write(str(datetime.datetime.now()))
   for URL in search_list:
@@ -36,13 +35,15 @@ def scrape(logger_path):
       logger.write('Success.')
   logger.close()        
 
-def main():
+def main():	    
+  search_list = read_link("https://yunxi.s3.us-east-2.amazonaws.com/search/search_link.txt")
+  for i in search_list:
+    print(i)
+  logger_path = "my_log.txt"
   i=0
-  logger_path = "my_log.txt"	    
   while(i>=0):
-    scrape(logger_path)
+    scrape(logger_path, search_list)
     print(str(i)+'\n')
-    i+=1
-
+    i+=1  
 if __name__ == "__main__":
   main()
